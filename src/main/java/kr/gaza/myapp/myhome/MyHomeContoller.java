2a376e6b11fa6d02a799179dd6a320f507a161b5
alstjd906 <alstjd906@gmail.com>
2019-11-15 16:57:38 +0900

    1

--- src/main/java/kr/gaza/myapp/myhome/MyHomeContoller.java
+++ src/main/java/kr/gaza/myapp/myhome/MyHomeContoller.java
@@ -1,24 +1,33 @@
 package kr.gaza.myapp.myhome;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MyHomeContoller {
 	@Autowired
 	SqlSession sqlSession;
-	@RequestMapping("/JSP/mypage/myhome")
+	@RequestMapping(value="/JSP/mypage/myhome", method=RequestMethod.GET)
 	public ModelAndView myHome(HttpServletRequest req) {
 		MyHomeDAOInterface dao = sqlSession.getMapper(MyHomeDAOInterface.class);
-		MyHomeVO vo=  dao.memberRecord(memberNum); 
+		HttpSession sess = req.getSession();
+		MyHomeVO vo=  dao.memberRecord((int)sess.getAttribute("memberNum")); 
 		
 		ModelAndView mav = new ModelAndView();
-		mav.setViewName("JSP/mypage/myhome");
+		if(vo!=null) {
+			mav.addObject("vo",vo);
+			mav.setViewName("JSP/mypage/myhome");
+		}else {
+			mav.setViewName("redirect:/");
+		}
+		
 		
 		return mav;
 	}
