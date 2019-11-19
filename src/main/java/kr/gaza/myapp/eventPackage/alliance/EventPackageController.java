14af1d232b2086111775d1e62c2843ee6fb219ea
DongHanKim95 <kd_h@naver.com>
2019-11-19 17:11:44 +0900

    이벤트 리스트,디테일
    ㅁㄴㅇㅁㅇ

--- src/main/java/kr/gaza/myapp/eventPackage/alliance/EventPackageController.java
+++ src/main/java/kr/gaza/myapp/eventPackage/alliance/EventPackageController.java
@@ -2,6 +2,8 @@ package kr.gaza.myapp.eventPackage.alliance;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,17 +11,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class EventPackageController {
+	public class EventPackageController {
 	@Autowired
 	SqlSession sqlSession;
-	@RequestMapping("/JSP/eventNpackage/best_package")
+	@RequestMapping("/JSP/eventNpackage/event_list")
 	public ModelAndView EventList() {
 		EPallianceInterface dao = sqlSession.getMapper(EPallianceInterface.class);
 		List<AllianceVO> lst = dao.getAllRecord();
 		
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("lst",lst);
-		mav.setViewName("/JSP/eventNpackage/best_package");
+		mav.setViewName("JSP/eventNpackage/event_list");
+		return mav;
+	}
+	
+	@RequestMapping("/JSP/eventNpackage/event_detail")
+	public ModelAndView DetailList(HttpServletRequest req) {
+		EPallianceInterface dao = sqlSession.getMapper(EPallianceInterface.class);
+		AllianceVO vo = new AllianceVO();
+		vo.setEventNum(Integer.parseInt(req.getParameter("eventNum")));
+		dao.eventSelect(vo);
+		
+		List<AllianceVO> lst = dao.getAllRecord();
+		
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("vo",vo);
+		mav.addObject("lst",lst);
+		mav.setViewName("JSP/eventNpackage/event_detail");
+		
 		return mav;
 	}
 }
