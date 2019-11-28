3bdb51108321924b52f54c34c939273cffbd3a01
woonas <woonhajung@gmail.com>
2019-11-28 16:05:05 +0900

    홈페이지에서 부킹2 완료

--- src/main/java/kr/gaza/myapp/HomeController.java
+++ src/main/java/kr/gaza/myapp/HomeController.java
@@ -1,5 +1,8 @@
 package kr.gaza.myapp;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 
@@ -15,11 +18,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import kr.gaza.myapp.aviation.flight.FlightVO;
+import kr.gaza.myapp.aviation.product.ProductVO;
 import kr.gaza.myapp.board.noticeBoard.NoticeBoardVO;
 import kr.gaza.myapp.board.reviewBoard.ReviewBoardVO;
 import kr.gaza.myapp.eventPackage.AllianceVO;
 import kr.gaza.myapp.eventPackage.EPtravelInterface;
 import kr.gaza.myapp.eventPackage.TravelVO;
+import kr.gaza.myapp.flight.booking.BookingInterface;
+import kr.gaza.myapp.flight.booking.BookingVO;
+import kr.gaza.myapp.flight.booking.JourneyVO;
 
 @Controller
 public class HomeController {
@@ -29,17 +37,17 @@ public class HomeController {
 	public ModelAndView home(Locale locale, Model model) {	
 		HomeInterface dao = sqlSession.getMapper(HomeInterface.class);
 		ModelAndView mav = new ModelAndView();
-		//√ﬂ√µªÛ«∞ ±€ ∞°¡Æø¿±‚
+		//Ï∂îÏ≤úÏÉÅÌíà Í∏Ä Í∞ÄÏ†∏Ïò§Í∏∞
 		EPtravelInterface recommandDao = sqlSession.getMapper(EPtravelInterface.class);
 		List<TravelVO> recommandList = recommandDao.getAllRecord();
 		
-		//¿Ã∫•∆Æ ±€ ∞°¡Æø¿±‚
+		//Ïù¥Î≤§Ìä∏ Í∏Ä Í∞ÄÏ†∏Ïò§Í∏∞
 		List<AllianceVO> eventList = dao.getEvent();
 		
-		//∞¯¡ˆªÁ«◊ ±€ ∞°¡Æø¿±‚
+		//Í≥µÏßÄÏÇ¨Ìï≠ Í∏Ä Í∞ÄÏ†∏Ïò§Í∏∞
 		List<NoticeBoardVO> noticeList = dao.getNotice();
 		
-		//¿ÃøÎ»ƒ±‚ ±€ ∞°¡Æø¿±‚
+		//Ïù¥Ïö©ÌõÑÍ∏∞ Í∏Ä Í∞ÄÏ†∏Ïò§Í∏∞
 		List<ReviewBoardVO> reviewList = dao.getReview();
 		
 		
@@ -52,32 +60,105 @@ public class HomeController {
 	}
 	@RequestMapping(value = "/JSP/simpleBooking")
 	public ModelAndView simpleBooking(
-			@RequestParam("airportFrom-1") String airportFrom,
-			@RequestParam("airportTo-1") String airportTo,
-			@RequestParam("fromDate") String fromDate,
-			@RequestParam("toDate") String toDate,
-			@RequestParam("trip-type") String flight_type,
-			@RequestParam("num-of-passengers") String numOfPassengers,
-			@RequestParam("class-type") String seatType,
-			HttpServletRequest req
+			@RequestParam("airportFrom-1") String airportFrom2,
+			@RequestParam("airportTo-1") String airportTo2,
+			@RequestParam("fromDate") String fromDate2,
+			@RequestParam("toDate") String toDate2,
+			@RequestParam("trip-type") String flight_type2,
+			@RequestParam("num-of-passengers") String numOfPassengers2,
+			@RequestParam("class-type") String seatType2,
+			HttpServletRequest request
 			) {	
-		
-		String flightDate = fromDate+"&"+toDate;
-		
-		HttpSession sess = req.getSession();
-		
-		sess.setAttribute("flight-type", flight_type);
-		sess.setAttribute("airportFrom", airportFrom);
-		sess.setAttribute("airportTo", airportTo);
-		sess.setAttribute("flightDate", flightDate);
-		sess.setAttribute("numOfPassengers", numOfPassengers);
-		sess.setAttribute("seatType", seatType);
-		
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("JSP/flight/booking/booking2");
-		
-		return mav;
-	}
 	
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("JSP/flight/booking/booking2");
+        
+        BookingVO bookingVO = new BookingVO();
+        bookingVO.setJourneyType(request.getParameter("flight-type"));
+        String[] airportFrom = {airportFrom2, airportTo2};
+        String[] airportTo = {airportTo2, airportFrom2};
+        String[] flightDate = {fromDate2, toDate2};
+        String numOfPassengers = numOfPassengers2;
+        String seatType = seatType2;
+        for (int i = 0; i < airportFrom.length; i++) {
+            JourneyVO journeyVO = new JourneyVO();
+            JourneyVO journeyVO2 = new JourneyVO();
+            journeyVO.setAirportFrom(airportFrom[i]);
+            journeyVO.setAirportTo(airportTo[i]);
+            journeyVO.setNumOfPassengers(numOfPassengers);
+            journeyVO.setFlightClass(seatType);
+            flightDate[i] = flightDate[i].replace("/","-");
+            
+                journeyVO.setFlightDate(flightDate[i]);
+                bookingVO.getJourneyList().add(journeyVO);
+            
+        }
+
+        BookingInterface bookingDAO = sqlSession.getMapper(BookingInterface.class);
+        List<JourneyVO> journeyList = bookingVO.getJourneyList();
+        for (int i = 0; i < journeyList.size(); i++) {
+            String from = journeyList.get(i).getAirportFromIATA();
+            String to = journeyList.get(i).getAirportToIATA();
+            bookingVO.getProductList().add(bookingDAO.getProductVO(from, to));
+        }
+        
+        List<ProductVO> productList = bookingVO.getProductList();
+        for (int i = 0; i < productList.size(); i++)
+            bookingVO.getFlightList().add(bookingDAO.getFlightVO(productList.get(i).getProductNum(), journeyList.get(i).getFlightDate()));
+
+        for (int i = 0; i < bookingVO.getFlightList().size(); i++) {
+            List<FlightVO> flightList = bookingVO.getFlightList().get(i);
+            bookingVO.getSeatLeftList().add(bookingDAO.getSeatLeft(flightList));
+        }
+
+        for (JourneyVO journeyVO : bookingVO.getJourneyList())
+            journeyVO.getFlightDate().replace("-","/");
+
+        request.setAttribute("bookingVO", bookingVO);
+         request.setAttribute("journey", new String[]{"Í∞ÄÎäî", "Ïò§Îäî"});
+
+        request.setAttribute("cheapestPriceList", getCheapestPrice(bookingVO));
+        return modelAndView;
+	}
+	 private List<List<Integer>> getCheapestPrice(BookingVO bookingVO) {
+	        int price = 0;
+	        double temp = 0;
+	        List<List<Integer>> priceList = new ArrayList<>();
+
+	        BookingInterface bookingDAO = sqlSession.getMapper(BookingInterface.class);
+	        BookingVO vo = new BookingVO();
+	        List<JourneyVO> journeyList = bookingVO.getJourneyList();
+	        for (int i = 0; i < journeyList.size(); i++) {
+	            String from = journeyList.get(i).getAirportFromIATA();
+	            String to = journeyList.get(i).getAirportToIATA();
+	            vo.getProductList().add(bookingDAO.getProductVO(from, to));
+	        }
+	        List<ProductVO> productList = vo.getProductList();
+	        for (int i = 0; i < productList.size(); i++) {
+	            for (int dateMod = -3; dateMod <= 3; dateMod++) {
+	                String journeyDate = journeyList.get(i).getFlightDate();
+	                int targetYear = Integer.parseInt(journeyDate.substring(0,4));
+	                int targetMonth = Integer.parseInt(journeyDate.substring(5,7))-1;
+	                int targetDate = Integer.parseInt(journeyDate.substring(8,10))+dateMod;
+	                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	                Calendar calendar = Calendar.getInstance();
+	                calendar.set(targetYear, targetMonth, targetDate);
+	                vo.getFlightList().add(bookingDAO.getFlightVO(productList.get(i).getProductNum(), simpleDateFormat.format(calendar.getTime())));
+	            }
+	            List<List<FlightVO>> flightVOList = vo.getFlightList();
+	            priceList.add(new ArrayList<>());
+	            //Êè∂ÏèâÏòôÈáéÍ∫øÎ´ñÔøΩÎè©ÔøΩÎ§ÉÂç†ÔøΩ
+	            temp = productList.get(i).getPrice() * productList.get(i).getProductSale();
+	            for (int j = 0; j < flightVOList.size(); j++) {
+	                for (int k = 0; k < flightVOList.get(j).size(); k++) {
+	                    if (price == 0 || temp * flightVOList.get(j).get(k).getFlightSale() < price)
+	                        price = (int) Math.round(temp * flightVOList.get(j).get(k).getFlightSale() / 100) * 100;
+	                }
+	                priceList.get(i).add(j, price);
+	                price = 0;
+	            }
+	        }
+	        return priceList;
+	    }
 }
 
